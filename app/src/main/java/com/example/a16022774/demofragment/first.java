3ebe6bcbb6184608dadc1f513525802a68aea38e
shawnwong65 <16022774@rp.edu.sg>
2018-06-04 11:07:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022774/demofragment/first.java
@@ -0,0 +1,47 @@
+package com.example.a16022774.demofragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class first extends Fragment {
+
+    Button btnAddText;
+    TextView tvFrag1;
+
+
+    public first() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        tvFrag1 = (TextView) view.findViewById(R.id.tvFrag1);
+        btnAddText = (Button) view.findViewById(R.id.btnAddTextFrag1);
+
+        btnAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = tvFrag1.getText().toString() + "\n" + "New Data";
+                tvFrag1.setText(data);
+            }
+        });
+
+        return view;
+    }
+
+}
